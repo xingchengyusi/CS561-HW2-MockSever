7a28662e7c79e7ba8e78beabdf9630b93f70f947
Chengzhi <zsxxxzks1314@gmail.com>
2022-10-23 13:53:52 -0700

    Mock

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+var path = require('path')
+const fs = require('fs')
+
+const port = 3000
+
+const getData = function (request, response) {
+    let dataFile = path.join(__dirname, 'data.json')
+    let res = JSON.parse(fs.readFileSync(dataFile, 'utf-8'))
+    // setTimeOut(function() {
+        return response.send(res)
+    // }, 100)
+}
+
+app.get('/api/v1/weather', getData)
+
+app.listen(port, function () {
+    console.log('mock app listening at http://localhost:3000');
+})
